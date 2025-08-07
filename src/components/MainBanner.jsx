ad65d73d27d6516e978d570afa7b3a1779bdda37
tarun0714 <manipalmemories1407@gmail.com>
2025-08-08 01:55:21 +0530

    adding text to image

---
+++ src/components/MainBanner.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const MainBanner = () => {
+  return (
+    <div className='relative'>
+        <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block'/>
+        <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden'/>
+        <div>
+          <h1>Freshness You Can Trust, Savings You Will Love!</h1>
+        </div>
+        <div>
+          <Link to={"/products"} className = 'group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>Shop now
+          <img className ='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="arrow"/>
+          </Link>
+          <Link to={"/products"} className = 'group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer'>Explore deals
+          <img className ='transition group-hover:translate-x-1' src={assets.black_arrow_icon} alt="arrow"/>
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+export default MainBanner
