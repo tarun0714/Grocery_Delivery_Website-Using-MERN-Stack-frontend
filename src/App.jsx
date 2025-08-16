7d9773c8c5eb08159c1e57339abb83abc42e8583
tarun0714 <manipalmemories1407@gmail.com>
2025-08-17 01:40:20 +0530

    best seller section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
 
@@ -10,6 +11,8 @@ const App = () => {
   return (
     <div>
       {isSellerPath ? null : <NavBar/> }
+
+      <Toaster />
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home/>} />
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,7 +8,7 @@ const Categories = () => {
 
   return (
     <div className='mt-16'>
-        <p className='text-2x1 md:text-3x1 font-medium'>Categories</p>
+        <p className='text-2xl md:text-3xl font-medium'>Categories</p>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6'>
 
             {categories.map((category, index)=>(
