a431681af6b81b635a2c54dbfa7208cff3d2b294
tarun0714 <manipalmemories1407@gmail.com>
2025-08-19 00:47:43 +0530

    Categories clickable

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -17,6 +17,7 @@ export const AppContextProvider = ({children})=>{
     const [products, setProducts] = useState([])
 
     const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setSearchQuery] = useState({})
 
     //Fetch All Products
     const fetchProducts = async () => {
@@ -61,7 +62,7 @@ export const AppContextProvider = ({children})=>{
         fetchProducts()
     },[])
 
-    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems}
+    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery}
     
     return <AppContext.Provider value = {value}>
         {children}
